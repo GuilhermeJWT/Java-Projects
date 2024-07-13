d86a15de3b8380fde2f4516d55d57dba8468bba7
GuilhermeJWT <guiromanno@gmail.com>
2024-07-13 16:16:11 -0300

    ADD: Streams - map - mais exemplo

--- api-streams/src/map/StreamsMapEX1Integer.java
+++ api-streams/src/map/StreamsMapEX1Integer.java
@@ -2,6 +2,7 @@ package map;
 
 import java.util.List;
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class StreamsMapEX1Integer {
 
@@ -9,8 +10,18 @@ public class StreamsMapEX1Integer {
 
         List<Integer> list = Arrays.asList(1, 5, 9, 15, 19);
 
+        System.out.println("----------------------------Exemplo 1------------------------------------------");
+
         /*Multiplica todos os números da lista por 3 = 3, 15, 27, 45, 57*/
         list.stream().map(numeros -> numeros * 3).forEach(System.out::println);
+
+        System.out.println("----------------------------Exemplo 2------------------------------------------");
+
+        String stringUnica = list.stream()
+                .map(String::valueOf) // Transforma cada item em STRING
+                .collect(Collectors.joining("-")); // Junta todas as Strings em uma única, separada por '-'
+
+        System.out.println(stringUnica); /*Resultado: 1-5-9-15-19*/
     }
 
 }
--- api-streams/src/map/StreamsMapEX2Strings.java
+++ api-streams/src/map/StreamsMapEX2Strings.java
@@ -8,18 +8,19 @@ public class StreamsMapEX2Strings {
 
     public static void main(String[] args) {
 
-        /*Exemplo 1*/
         List<String> listString = Arrays.asList("Guilherme", "Santos", "Java", "Streams");
+
+        System.out.println("----------------------------Exemplo 1------------------------------------------");
 
         /*Transforma todas as letras em Maiusculo = [GUILHERME, SANTOS, JAVA, STREAMS]*/
         List<String> upString = listString.stream().map(String::toUpperCase).collect(Collectors.toList());
 
         System.out.println(upString);
-        System.out.println("------------------------------------------------------");
 
-        /*Exemplo 2*/
+        System.out.println("----------------------------Exemplo 2------------------------------------------");
         /*Conta a quantidade de caracteres de cada palavra da lista = 9, 6, 4, 7*/
         listString.stream().map(caracteres -> caracteres.length()).forEach(System.out::println);
+
     }
 
 }
