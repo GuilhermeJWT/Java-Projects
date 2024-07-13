cadf84f54a63023b6e8995a8a43aa651b3f967d9
GuilhermeJWT <guiromanno@gmail.com>
2024-07-13 13:56:02 -0300

    ADD: Streams - String length

--- api-streams/src/map/StramsMapEX2Strings.java
+++ api-streams/src/map/StramsMapEX2Strings.java
@@ -8,12 +8,18 @@ public class StramsMapEX2Strings {
 
     public static void main(String[] args) {
 
+        /*Exemplo 1*/
         List<String> listString = Arrays.asList("Guilherme", "Santos", "Java", "Streams");
 
         /*Transforma todas as letras em Maiusculo = [GUILHERME, SANTOS, JAVA, STREAMS]*/
         List<String> upString = listString.stream().map(String::toUpperCase).collect(Collectors.toList());
 
         System.out.println(upString);
+        System.out.println("------------------------------------------------------");
+
+        /*Exemplo 2*/
+        /*Conta a quantidade de caracteres de cada palavra da lista = 9, 6, 4, 7*/
+        listString.stream().map(caracteres -> caracteres.length()).forEach(System.out::println);
     }
 
 }
