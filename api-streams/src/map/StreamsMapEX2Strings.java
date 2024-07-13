33236351f1c5ec8744a25d26e2840a5b76712529
GuilhermeJWT <guiromanno@gmail.com>
2024-07-13 14:28:37 -0300

    ADD: Streams - Entidade - Percorrendo uma lista com campos

--- api-streams/src/map/StreamsMapEX2Strings.java
+++ api-streams/src/map/StreamsMapEX2Strings.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
-public class StramsMapEX2Strings {
+public class StreamsMapEX2Strings {
 
     public static void main(String[] args) {
 
--- api-streams/src/map/StreamsMapPessoas.java
+++ api-streams/src/map/StreamsMapPessoas.java
@@ -1,4 +1,33 @@
 package map;
 
+import models.Pessoa;
+import models.PessoaConvertida;
+
+import java.math.BigDecimal;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class StreamsMapPessoas {
+
+    public static void main(String[] args) {
+
+        List<Pessoa> pessoas = Arrays.asList(
+                new Pessoa(25, "Guilherme", new BigDecimal(1000)),
+                new Pessoa(33, "Java", new BigDecimal(3500)),
+                new Pessoa(15, "Streams", new BigDecimal(750))
+        );
+
+        List<PessoaConvertida> result = pessoas.stream().map(converter -> {
+            PessoaConvertida pessoaConvertida = new PessoaConvertida();
+            pessoaConvertida.setIdadeConvertida(converter.getIdade());
+            pessoaConvertida.setNomeConvertido(converter.getNome());
+            pessoaConvertida.setSalarioConvertido(String.valueOf(converter.getSalario()));
+
+            return pessoaConvertida;
+        }).collect(Collectors.toList());
+
+        System.out.println(result);
+
+    }
 }
